9bfcf3c4f82c4cde2430ad7a1fd660b75986118c
Churnobyl <tjdcjfals@gmail.com>
2024-05-20 00:32:50 +0900

    test

--- pistachio/src/main/java/com/ssafy/pistachio/PistachioApplication.java
+++ pistachio/src/main/java/com/ssafy/pistachio/PistachioApplication.java
@@ -1,17 +1,31 @@
 package com.ssafy.pistachio;
 
-import com.ssafy.pistachio.config.WebSecurityConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
-public class PistachioApplication {
+public class PistachioApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(PistachioApplication.class);
+
+	@Value("${spring.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.redis.port}")
+	private int redisPort;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PistachioApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Redis Host: {}", redisHost);
+		logger.info("Redis Port: {}", redisPort);
+	}
 }
